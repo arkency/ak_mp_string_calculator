c245284fcb4e1200b3feee53cd20a8999ab3657f
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2014-06-04 16:43:04 +0200

    initial truth test, ping @mpraglowski

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 ak_mp_string_calculator
 =======================
+
+http://osherove.com/tdd-kata-1/
---
+++ string_calculator_test.rb
@@ -0,0 +1,9 @@
+require 'test/unit'
+
+class StringCalculatorTest < Test::Unit::TestCase
+  
+  def test_truth
+    assert_equal(false, true)
+  end
+end
+

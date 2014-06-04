29e715dcd9c80fe7cfd0c65144d0b92640b15c22
Mirosław Pragłowski <m@praglowski.com>
2014-06-04 23:32:03 +0200

    truth test passed, test for empty string

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -3,7 +3,12 @@ require 'test/unit'
 class StringCalculatorTest < Test::Unit::TestCase
   
   def test_truth
-    assert_equal(false, true)
+    assert_equal(true, true)
+  end
+
+  def test_empty_string_should_sum_to_zero
+    calc = Calculator.new
+    assert_equal(calc.add(""), 0)
   end
 end
 

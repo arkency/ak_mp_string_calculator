e4d918b38d256dd5c64a57f1b3938231122f24cc
Mirosław Pragłowski <m@praglowski.com>
2014-06-04 23:39:11 +0200

    test for single & double numbers argument string

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -10,5 +10,16 @@ class StringCalculatorTest < Test::Unit::TestCase
     calc = Calculator.new
     assert_equal(calc.add(""), 0)
   end
+
+  def test_single_number_should_sum_to_itself
+    calc = Calculator.new
+    assert_equal(calc.add("123"), 123)
+  end
+
+  def test_1_plus_2_should_sum_to_3
+    calc = Calculator.new
+    assert_equal(calc.add("1,2"), 3)
+  end
+
 end
 

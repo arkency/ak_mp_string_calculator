89792faf6873e7b0ef18ec237ff883d0a61b1cc4
Mirosław Pragłowski <m@praglowski.com>
2014-06-06 15:03:46 +0200

    fixed arguments order in assert_equal (was actual,expected, should be expected,actual), added test for giving delimiters definition in first line of add arguments

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -8,29 +8,32 @@ class StringCalculatorTest < Test::Unit::TestCase
 
   def test_empty_string_should_sum_to_zero
     calc = Calculator.new
-    assert_equal(calc.add(""), 0)
+    assert_equal(0, calc.add(""))
   end
 
   def test_single_number_should_sum_to_itself
     calc = Calculator.new
-    assert_equal(calc.add("123"), 123)
+    assert_equal(123, calc.add("123"))
   end
 
   def test_1_plus_2_should_sum_to_3
     calc = Calculator.new
-    assert_equal(calc.add("1,2"), 3)
+    assert_equal(3, calc.add("1,2"))
   end
 
   def test_newlines_are_good_delimiters_too
     calc = Calculator.new
-    assert_equal(calc.add("1\n2,3"), 6)
+    assert_equal(6, calc.add("1\n2,3"))
+  end
+
+  def test_delimiters_given_in_first_line_of_args_should_be_used
+    calc = Calculator.new
+    assert_equal(3, calc.add("//;\n1;2"))
   end
 
 end
 
 class Calculator
-  delimiters = ["\n",","]
-
   def add(expression)
     expression.split(/[\n,]/).map(&:to_i).inject(0, :+)
   end
